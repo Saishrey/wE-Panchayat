0388c7f6f9c70dcca813104f037e087f5f69d48c
Saishrey <86342945+Saishrey@users.noreply.github.com>
2023-03-11 17:33:20 +0530

    Validators for Password and Pin Code added

--- lib/auth/signup.dart
+++ lib/auth/signup.dart
@@ -44,6 +44,8 @@ class _SignUpState extends State<SignUp> {
 
   DateTime _selectedDate = DateTime.now();
 
+  String _password = "";
+
   @override
   Widget build(BuildContext context) {
     TextStyle textStyle = Theme.of(context).textTheme.titleMedium!;
@@ -150,7 +152,7 @@ class _SignUpState extends State<SignUp> {
                                   child: TextFormField(
                                     validator: (value) {
                                       if (value!.isEmpty) {
-                                        return "Enter  LastName ";
+                                        return "Enter LastName ";
                                       }
                                       return null;
                                     },
@@ -199,6 +201,46 @@ class _SignUpState extends State<SignUp> {
                               ),
                               decoration: InputDecoration(
                                 labelText: 'Address',
+                                filled: true,
+                                fillColor: Color(0xffF6F6F6),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide(
+                                    color: Color(0xffBDBDBD),
+                                  ),
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide(
+                                    color: Color(0xffBDBDBD),
+                                  ),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide(
+                                    color: Theme.of(context).primaryColor,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 16),
+                            TextFormField(
+                              validator: (value) {
+                                if (value!.isEmpty || value.length < 6) {
+                                  return "Enter 6 Digit Pincode   ";
+                                }
+                                return null;
+                              },
+                              keyboardType: TextInputType.number,
+                              inputFormatters: [
+                                new LengthLimitingTextInputFormatter(6),
+                              ],
+                              style: TextStyle(
+                                color: Colors.black54,
+                                fontFamily: 'Poppins-Bold',
+                              ),
+                              decoration: InputDecoration(
+                                labelText: 'Pincode',
                                 filled: true,
                                 fillColor: Color(0xffF6F6F6),
                                 border: OutlineInputBorder(
@@ -298,7 +340,7 @@ class _SignUpState extends State<SignUp> {
                                           _selectedVillage =
                                               newValue.toString();
                                           debugPrint(
-                                              "Selected taluka =  $_selectedVillage");
+                                              "Selected village =  $_selectedVillage");
                                         });
                                       },
                                     ),
@@ -324,6 +366,46 @@ class _SignUpState extends State<SignUp> {
                                 ),
                                 trailing: Icon(Icons.keyboard_arrow_down),
                                 onTap: _pickDate,
+                              ),
+                            ),
+                            SizedBox(height: 16),
+                            TextFormField(
+                              validator: (value) {
+                                if (value!.isEmpty || value.length < 10) {
+                                  return "Enter Valid Phone Number ";
+                                }
+                                return null;
+                              },
+                              keyboardType: TextInputType.number,
+                              inputFormatters: [
+                                new LengthLimitingTextInputFormatter(10),
+                              ],
+                              style: TextStyle(
+                                color: Colors.black54,
+                                fontFamily: 'Poppins-Bold',
+                              ),
+                              decoration: InputDecoration(
+                                labelText: 'Phone Number',
+                                filled: true,
+                                fillColor: Color(0xffF6F6F6),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide(
+                                    color: Color(0xffBDBDBD),
+                                  ),
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide(
+                                    color: Color(0xffBDBDBD),
+                                  ),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide(
+                                    color: Theme.of(context).primaryColor,
+                                  ),
+                                ),
                               ),
                             ),
                             SizedBox(height: 16),
@@ -372,6 +454,15 @@ class _SignUpState extends State<SignUp> {
                                 if (value!.isEmpty) {
                                   return "Enter Password   ";
                                 }
+                                else if(!isPasswordValid(value)) {
+                                  return "Password must have:\n"
+                                      "At least 8 characters long.\n"
+                                  "At least one uppercase letter.\n"
+                                  "At least one lowercase letter.\n"
+                                  "At least one number.\n"
+                                  "At least one special character.";
+                                }
+                                _password = value;
                                 return null;
                               },
                               style: TextStyle(
@@ -417,6 +508,23 @@ class _SignUpState extends State<SignUp> {
                             ),
                             SizedBox(height: 16),
                             TextFormField(
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return "Enter Password   ";
+                                }
+                                else if(!isPasswordValid(value)) {
+                                  return "Password must have:\n"
+                                      "At least 8 characters long.\n"
+                                      "At least one uppercase letter.\n"
+                                      "At least one lowercase letter.\n"
+                                      "At least one number.\n"
+                                      "At least one special character.";
+                                }
+                                else if(value != _password) {
+                                  return "Passwords do not match.";
+                                }
+                                return null;
+                              },
                               style: TextStyle(
                                 color: Colors.black54,
                                 fontFamily: 'Poppins-Bold',
@@ -438,86 +546,6 @@ class _SignUpState extends State<SignUp> {
                                         : Icons.visibility_off,
                                   ),
                                 ),
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                  borderSide: BorderSide(
-                                    color: Color(0xffBDBDBD),
-                                  ),
-                                ),
-                                enabledBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                  borderSide: BorderSide(
-                                    color: Color(0xffBDBDBD),
-                                  ),
-                                ),
-                                focusedBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                  borderSide: BorderSide(
-                                    color: Theme.of(context).primaryColor,
-                                  ),
-                                ),
-                              ),
-                            ),
-                            SizedBox(height: 16),
-                            TextFormField(
-                              validator: (value) {
-                                if (value!.isEmpty) {
-                                  return "Enter 6 Digit Pincode   ";
-                                }
-                                return null;
-                              },
-                              keyboardType: TextInputType.number,
-                              inputFormatters: [
-                                new LengthLimitingTextInputFormatter(6),
-                              ],
-                              style: TextStyle(
-                                color: Colors.black54,
-                                fontFamily: 'Poppins-Bold',
-                              ),
-                              decoration: InputDecoration(
-                                labelText: 'Pincode',
-                                filled: true,
-                                fillColor: Color(0xffF6F6F6),
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                  borderSide: BorderSide(
-                                    color: Color(0xffBDBDBD),
-                                  ),
-                                ),
-                                enabledBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                  borderSide: BorderSide(
-                                    color: Color(0xffBDBDBD),
-                                  ),
-                                ),
-                                focusedBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                  borderSide: BorderSide(
-                                    color: Theme.of(context).primaryColor,
-                                  ),
-                                ),
-                              ),
-                            ),
-                            SizedBox(height: 16),
-                            TextFormField(
-                              validator: (value) {
-                                if (value!.isEmpty || value.length < 10) {
-                                  return "Enter Valid Phone Number ";
-                                }
-                                return null;
-                              },
-                              keyboardType: TextInputType.number,
-                              inputFormatters: [
-                                new LengthLimitingTextInputFormatter(10),
-                              ],
-                              style: TextStyle(
-                                color: Colors.black54,
-                                fontFamily: 'Poppins-Bold',
-                              ),
-                              decoration: InputDecoration(
-                                labelText: 'Phone Number',
-                                filled: true,
-                                fillColor: Color(0xffF6F6F6),
                                 border: OutlineInputBorder(
                                   borderRadius: BorderRadius.circular(10),
                                   borderSide: BorderSide(
@@ -623,5 +651,35 @@ class _SignUpState extends State<SignUp> {
         _selectedDate = date;
       });
     }
+  }
+
+  bool isPasswordValid(String password) {
+    // Check if password is at least 8 characters long
+    if (password.length < 8) {
+      return false;
+    }
+
+    // Check if password contains at least one uppercase letter
+    if (password.toLowerCase() == password) {
+      return false;
+    }
+
+    // Check if password contains at least one lowercase letter
+    if (password.toUpperCase() == password) {
+      return false;
+    }
+
+    // Check if password contains at least one number
+    if (!password.contains(new RegExp(r'[0-9]'))) {
+      return false;
+    }
+
+    // Check if password contains at least one special character
+    if (!password.contains(new RegExp(r'[!@#$%^&*(),.?":{}|<>]'))) {
+      return false;
+    }
+
+    // If all checks pass, the password is valid
+    return true;
   }
 }
